d55a74943f251062d00f562f8f19e81fd1622126
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-07 21:57:06 -0700

    passed first exercise, just had to pass reversed = True for sorted

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,10 +1,28 @@
 from typing import Tuple, List 
 Answer = Tuple[int, int]
 
-def get_indices_of_item_weights(weights: List[int], length: int, limit: int) -> Answer:
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+"""
+We add keys using setdefault since we want to keep the oldest key, doing 
+regular enumerate will over write with the newest one for duplicates
 
-    return None
+"""
+
+def get_indices_of_item_weights(weights: List[int], length: int, limit: int) -> Answer:
+	# fail on list shorter than 2 
+	if length <= 1:
+		return None
+	# empty dict 
+	index_map = {}
+	for index_one, n in enumerate(weights):
+
+		m = limit - n
+		try:
+			index_two = index_map[m]
+		except KeyError:
+			index_map.setdefault(n, index_one)
+		else:
+			return tuple(sorted([index_one, index_two], reverse=True))
+
+weights_2 = [4, 4, 4]
+answer_2 = get_indices_of_item_weights(weights_2, 3, 8)
+print(answer_2)
