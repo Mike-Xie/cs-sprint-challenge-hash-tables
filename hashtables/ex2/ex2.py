e1c1e60b02ee42e1c97ec51d05e5d86de9c5b256
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-06-07 22:29:02 -0700

    ex2 mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,6 +1,7 @@
 from typing import List
 
-#  Hint:  You may not need all of these.  Remove the unused functions.
+# First ticket is None, Something
+# Last ticket is Something, None 
 
 
 class Ticket:
@@ -11,12 +12,15 @@ class Ticket:
 
 def reconstruct_trip(tickets: List[Ticket], length: int) -> List[str]:
     route_dict = {}
-
     for i in tickets:
     	route_dict[i.source] = i.destination
-
-    print(route_dict)
-    return None
+    curr_stop = route_dict['NONE']
+    route_list = [curr_stop]
+    while curr_stop != 'NONE':
+    	curr_stop = route_dict[curr_stop]
+    	route_list.append(curr_stop)
+    return(route_list)
+    
 
 
 
@@ -24,5 +28,5 @@ ticket_1 = Ticket("NONE", "PDX")
 ticket_2 = Ticket("PDX", "DCA")
 ticket_3 = Ticket("DCA", "NONE")
 tickets = [ticket_1, ticket_2, ticket_3]
-reconstruct_trip(tickets, 3)
+print(reconstruct_trip(tickets, 3))
 expected = ["PDX", "DCA", "NONE"]
